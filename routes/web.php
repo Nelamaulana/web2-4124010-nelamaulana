0129fb60081f78c0208242447d30facfa16db7df
nela <nelaulifatulzahromaulana@gmail.com>
2026-03-06 12:36:06 +0700

    feat: inisiasi project Laravel – Nela Ulifatul Zahro Maulana

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/perkenalan', function () {
+    return '<h1>Halo! Nama saya Nela Ulifatul Zahro Maulana</h1>
+            <p>NIM: 4124010 | Prodi: Sistem Informasi</p>
+            <p>Saya siap belajar Laravel! 🚀</p>';
+});
